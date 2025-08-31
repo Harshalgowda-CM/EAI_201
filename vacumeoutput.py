1e1ac5af70bb9027071f523c6b83f348bd1f14d9
Harshalgowda-CM <harshalgm.eai24@chanakyauniversity.edu.in>
2025-08-31 11:58:11 +0530

    Create vacumeoutput.py

---
+++ vacumeoutput.py
@@ -0,0 +1,32 @@
+--- Vacuum Cleaner Menu ---
+1. Start Cleaning
+2. Turn Left
+3. Turn Right
+4. Dock Vacuum
+5. Stop Program
+Enter your choice (1-5): 1
+
+Vacuum Started
+1. Solid
+2. Liquid
+3. Back to Main Menu
+Choose the category (1-3): 1
+
+Choose the type of solid:
+1. Dust
+2. Rocks/Papers
+3. Others
+Enter your choice (1-3): 1
+
+Cleaning Category: Solid
+Item: Dust
+
+Best Shape Selected: Round
+Total Cells Cleaned: 100
+Time Taken: 100 steps
+
+Time Taken by Each Shape:
+Round: 100 steps
+Square: 200 steps
+Triangle: 300 steps
+Heptagon: 150.0 steps
