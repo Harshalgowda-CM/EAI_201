137b61761bfbfdcabcd9b9d771e5d4f23977f1c6
Harshalgowda-CM <harshalgm.eai24@chanakyauniversity.edu.in>
2025-08-31 11:56:46 +0530

    Create grading output.py

---
+++ grading output.py
@@ -0,0 +1,28 @@
+-Grading as per Final Results-
+Enter subject name and marks - Type 'done' when finished aploding marks.
+
+Enter The Subject Name (or type 'done' to quit): DSA
+Enter marks in DSA: 80
+DSA : 80 -> Grade B 
+
+Enter The Subject Name (or type 'done' to quit): AI and ML
+Enter marks in AI and ML: 95
+AI and ML : 95 -> Grade A 
+
+Enter The Subject Name (or type 'done' to quit): Maths
+Enter marks in Maths: 65
+Maths : 65 -> Grade D 
+
+Enter The Subject Name (or type 'done' to quit): German language
+Enter marks in German language: 70
+German language : 70 -> Grade C 
+
+Enter The Subject Name (or type 'done' to quit): Discreat maths
+Enter marks in Discreat maths: 78
+Discreat maths : 78 -> Grade C 
+
+Enter The Subject Name (or type 'done' to quit): done
+ Final Result
+Total Marks: 388
+Average: 77.6
+Final Grade: C
